cdd398ccaf2a01ae65ba2a3ac8157ab326832392
mrstruijk <mrstruijk@gmail.com>
2024-11-08 16:50:28 +0100

    Update DisableEditingPropertyDrawer.cs

--- Editor/Attributes/DisableEditingPropertyDrawer.cs
+++ Editor/Attributes/DisableEditingPropertyDrawer.cs
@@ -6,36 +6,25 @@ namespace SOSXR.EditorTools.Attributes
 {
     /// <summary>
     ///     From: https://gist.github.com/LotteMakesStuff/c0a3b404524be57574ffa5f8270268ea
-    ///     With additions from ChatGPT
+    ///     With additions from ChatGPT and Claude.
+    ///     Limitations: Does not disable '+' button for lists.
     /// </summary>
     [CustomPropertyDrawer(typeof(DisableEditingAttribute))]
     public class DisableEditingPropertyDrawer : PropertyDrawer
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            var wasEnabled = GUI.enabled;
-            GUI.enabled = false;
+            EditorGUI.BeginDisabledGroup(true);
 
             EditorGUI.PropertyField(position, property, label, true);
 
-            GUI.enabled = wasEnabled;
+            EditorGUI.EndDisabledGroup();
         }
 
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            var totalHeight = EditorGUI.GetPropertyHeight(property, label, true);
-
-            if (property.isArray && property.isExpanded)
-            {
-                for (var i = 0; i < property.arraySize; i++)
-                {
-                    var element = property.GetArrayElementAtIndex(i);
-                    totalHeight += EditorGUI.GetPropertyHeight(element, true) + EditorGUIUtility.standardVerticalSpacing;
-                }
-            }
-
-            return totalHeight;
+            return EditorGUI.GetPropertyHeight(property, label, true);
         }
     }
 }
